ae8f882bf0294ef35c99ee20fcfbd6250ff1730d
Jeremy Kwa <jkwa@ucsd.edu>
2017-03-06 12:13:47 -0800

    first working draft test 1

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      #flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      render :success
     else
-      flash.now[:error] = 'Cannot send message.'
+      #flash.now[:error] = 'Cannot send message.'
       render :new
     end
   end
