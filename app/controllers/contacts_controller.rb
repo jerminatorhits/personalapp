75464dec12ece2b7c972d5c959208bf10bedd70d
Jeremy Kwa <jkwa@ucsd.edu>
2017-03-06 04:05:54 -0800

    fixing mailer1

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,24 @@
-class ContactsController < ApplicationController
-  def new
+class ContactsController < ApplicationController 
+
+    def new
     @contact = Contact.new
+    if 
+    Contact.new(params[:contact]).deliver
+    redirect_to '/contacts/thanks', :alert => ["Yeah!"]
+    end
+   else
+
+   end
+
+
+   def create
+@contact = Contact.new(params[:contact])
+    if 
+   @contact.valid?
+@contact.deliver
+   end
+   else 
+  render :new
   end
 
-  def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
-    end
   end
-end
